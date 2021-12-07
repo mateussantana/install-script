3cf3d0aa0bc7a246b931635806e8c06a0b65e3ae
Mateus Santana <mateusssantana@gmail.com>
2021-12-07 20:30:26 -0300

    starting in install script

--- install.sh
+++ install.sh
@@ -5,6 +5,48 @@
 # ref: https://github.com/laravel/sail/blob/1.x/bin/sail
 
 # Verify OS
-echo $(uname -s)
+if  [[ ! "$(uname -s)" =~ "Linux" ]]; then
+    echo "Seems like you are not in a Linux environment."
+    echo -n "Are you want to proceed anyway? (y/n) "
+    read ANSWER
+    echo ""
 
-# sudo -v
+    if [[ ! $ANSWER =~ ^[yY]$ ]]; then
+        echo "Aborted!"
+        exit 0
+    fi
+fi
+
+# Verify Ubuntu distribution
+if  [[ ! "$(uname -v)" =~ "Ubuntu" ]]; then
+    echo "Seems like you are not using a Ubuntu distribution. This script just was tested on Ubuntu."
+    echo -n "Are you want to proceed anyway? (y/n) "
+    read ANSWER
+    if [[ ! $ANSWER =~ ^[yY]$ ]]; then
+        echo "Aborted!"
+        exit 0
+    fi
+
+    echo -n "Are you really sure? (y/n) "
+    read ANSWER
+    if [[ ! $ANSWER =~ ^[yY]$ ]]; then
+        echo "Aborted!"
+        exit 0
+    fi
+
+    echo ""
+fi
+
+# Confirm sudo privileges
+sudo -v
+if [ $? -ne 0 ]; then
+    echo "You need sudo privileges. Aborted!"
+    exit 1
+fi
+
+echo "Executing..."
+
+# Confirm to proceed 
+sudo apt-get update
+
+# Install dependencies
