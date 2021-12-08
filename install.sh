1acdafcd49fd0eca011c5267e3ce7ea437ec1e6d
Mateus Santana <mateusssantana@gmail.com>
2021-12-08 02:05:31 -0300

    refact: learning how to use colors and emojis

--- install.sh
+++ install.sh
@@ -3,31 +3,39 @@
 # ref: https://github.com/freekmurze/dotfiles/blob/main/installscript
 # ref: https://github.com/freekmurze/dotfiles/blob/main/bootstrap
 # ref: https://github.com/laravel/sail/blob/1.x/bin/sail
+# ref: https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# Setup colors
+RED=`printf '\033[31m'`
+GREEN=`printf '\033[32m'`
+YELLOW=`printf '\033[33m'`
+BLUE=`printf '\033[34m'`
+BOLD=`printf '\033[1m'`
+RESET=`printf '\033[m'`
+
+# Setup emojis
+EMOJI_CHECK=`printf '\U2705\n'`
+EMOJI_CROSS=`printf '\U274C\n'`
+EMOJI_WARNING=`printf '\U26A0\n'`
 
 # Verify OS
 if  [[ ! "$(uname -s)" =~ "Linux" ]]; then
-    echo "Seems like you are not in a Linux environment."
-    echo -n "Are you want to proceed anyway? (y/n) "
-    read ANSWER
-    echo ""
-
-    if [[ ! $ANSWER =~ ^[yY]$ ]]; then
-        echo "Aborted!"
-        exit 0
-    fi
+    echo "$EMOJI_CROSS ${RED}Seems like you are not in a Linux environment.${RESET}"
+    echo "Aborted!"
+    exit 1
 fi
 
 # Verify Ubuntu distribution
 if  [[ ! "$(uname -v)" =~ "Ubuntu" ]]; then
-    echo "Seems like you are not using a Ubuntu distribution. This script just was tested on Ubuntu."
-    echo -n "Are you want to proceed anyway? (y/n) "
+    echo "$EMOJI_WARNING ${BLUE}Seems like you are not using a Ubuntu distribution. This script just was tested on Ubuntu.${RESET}"
+    echo -n "${YELLOW}Are you want to proceed anyway? ${RESET}(y/n) "
     read ANSWER
     if [[ ! $ANSWER =~ ^[yY]$ ]]; then
         echo "Aborted!"
         exit 0
     fi
 
-    echo -n "Are you really sure? (y/n) "
+    echo -n "${YELLOW}Are you really sure? ${RESET}(y/n) "
     read ANSWER
     if [[ ! $ANSWER =~ ^[yY]$ ]]; then
         echo "Aborted!"
@@ -40,9 +48,18 @@ fi
 # Confirm sudo privileges
 sudo -v
 if [ $? -ne 0 ]; then
-    echo "You need sudo privileges. Aborted!"
+    echo "$EMOJI_CROSS ${RED}You need sudo privileges.${RESET}"
+    echo "Aborted!"
     exit 1
 fi
+
+if $(gits --version > /dev/null 2>&1); then
+    echo "$EMOJI_CHECK Git is already installed"
+else
+    echo "${RED}Git is not installed!"
+fi
+
+exit 0;
 
 echo "Executing..."
 
