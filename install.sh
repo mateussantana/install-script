0750fbaf77840b6ee99058d1e2fe2af39fd2b11b
Mateus Santana <mateusssantana@gmail.com>
2021-12-10 12:50:45 -0300

    feat: docker, docker-compose & ngrok installation

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ RESET=`printf '\033[m'`
 EMOJI_SUCCESS=`printf '\U2705\n'`
 EMOJI_ERROR=`printf '\U1F6A8\n'`
 EMOJI_WARNING=`printf '\U26A0\n'`
+EMOJI_REFRESH=`printf '\U1F504\n'`
 EMOJI_MAGIC_WAND=`printf '\U1FA84\n'`
 EMOJI_GIT=`printf '\U1F500\n'`
 EMOJI_DOCKER=`printf '\U1F433\n'`
@@ -51,46 +52,94 @@ if  [[ ! "$(uname -v)" =~ "Ubuntu" ]]; then
     echo ""
 fi
 
+# Script information
+printf "\n"
+printf "╔═════════════════════════════════════════════════════╗\n"
+printf "║        %s♣%s WELCOME TO MY INSTALLATION SCRIPT %s♣%s        ║\n" ${BOLD}${RED} ${RESET}${BOLD} ${RED} $RESET
+printf "║                                                     ║\n"
+printf "║ This script will do the following tasks:            ║\n"
+printf "║  → Install %sGit%s                                      ║\n" ${BOLD}${GREEN} $RESET
+printf "║  → Install %sDocker%s & %sdocker-compose%s                  ║\n" ${BOLD}${GREEN} $RESET ${BOLD}${GREEN} $RESET
+printf "║  → Install %sNgrok%s                                    ║\n" ${BOLD}${GREEN} $RESET
+printf "║  → Install %sZSH%s                                      ║\n" ${BOLD}${GREEN} $RESET
+printf "║  → Install %sOhMyZsh%s                                  ║\n" ${BOLD}${GREEN} $RESET
+printf "║  → Install %sOhMyZsh theme%s                            ║\n" ${BOLD}${GREEN} $RESET
+printf "║  → Install %sOhMyZsh plugins%s                          ║\n" ${BOLD}${GREEN} $RESET
+printf "╚═════════════════════════════════════════════════════╝\n"
+
+# Confirm to proceed
+echo -n "${YELLOW}Are you want to proceed? ${RESET}(y/n) "
+read ANSWER
+if [[ ! $ANSWER =~ ^[yY]$ ]]; then
+    echo "Ok. Aborted!"
+    exit 0
+else
+    echo ""
+fi
+
 # Confirm sudo privileges
 sudo -v
 if [ $? -ne 0 ]; then
     echo "$EMOJI_ERROR ${RED}You need sudo privileges.${RESET}"
     echo "Aborted!"
     exit 1
+else
+    echo ""
 fi
 
-# Confirm to proceed
-printf "\n"
-printf "╔═════════════════════════════════════════════════════╗\n"
-printf "║        %s♣ WELCOME TO MY INSTALLATION SCRIPT ♣%s        ║\n" $BOLD $RESET
-printf "║                                                     ║\n"
-printf "║ This script will do the following tasks:            ║\n"
-printf "║  → Install %sGit%s                                      ║\n" ${BOLD}${BLUE} $RESET
-printf "║  → Install %sDocker%s & %sdocker-compose%s                  ║\n" ${BOLD}${BLUE} $RESET ${BOLD}${BLUE} $RESET
-printf "║  → Install %sNgrok%s                                    ║\n" ${BOLD}${BLUE} $RESET
-printf "║  → Install %sZSH%s                                      ║\n" ${BOLD}${BLUE} $RESET
-printf "║  → Install %sOhMyZsh%s                                  ║\n" ${BOLD}${BLUE} $RESET
-printf "║  → Install %sOhMyZsh theme%s                            ║\n" ${BOLD}${BLUE} $RESET
-printf "║  → Install %sOhMyZsh plugins%s                          ║\n" ${BOLD}${BLUE} $RESET
-printf "╚═════════════════════════════════════════════════════╝\n"
-printf "\n"
-
-echo -n "${YELLOW}Are you want to proceed? ${RESET}(y/n) "
-read ANSWER
-if [[ ! $ANSWER =~ ^[yY]$ ]]; then
-    echo "Ok. Aborted!"
-    exit 0
-fi
+# Update and install dependencies
+CMD='sudo apt-get update && sudo apt-get install curl -y'
+echo "${GREEN}${CMD}${RESET}"
+eval $CMD || exit 2
+echo ""
 
 # Git
-if $(gits --version > /dev/null 2>&1); then
-    echo "$EMOJI_SUCCESS Git is already installed"
+if  $(git --version > /dev/null 2>&1); then
+    printf "%s Git is already installed\n" $EMOJI_SUCCESS
 else
-    echo "$EMOJI_GIT Git is not installed!"
+    printf "%s Installing %sGit%s...\n" $EMOJI_GIT $GREEN $RESET
+    CMD='sudo apt-get install git -y'
+    echo "${GREEN}${CMD}${RESET}"
+    $CMD || exit 2
+    echo ""
 fi
 
-echo " Executing..."
-exit 0;
+# Docker & Docker-compose
+if  $(docker --version > /dev/null 2>&1); then
+    printf "%s Docker is already installed\n" $EMOJI_DOCKER
+else
+    printf "%s Installing %sDocker%s...\n" $EMOJI_DOCKER $GREEN $RESET
+    CMD='curl -fsSL https://get.docker.com -o /tmp/get-docker.sh'
+    echo "${GREEN}${CMD}${RESET}"
+    $CMD || exit 2
+    CMD='sudo sh /tmp/get-docker.sh'
+    echo "${GREEN}${CMD}${RESET}"
+    $CMD || exit 2
+    CMD='sudo groupadd docker && sudo usermod -aG docker $USER'
+    echo "${GREEN}${CMD}${RESET}"
+    #$CMD || exit 2
+    CMD='sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+    echo "${GREEN}${CMD}${RESET}"
+    eval $CMD || exit 2
+    CMD='sudo chmod +x /usr/local/bin/docker-compose'
+    echo "${GREEN}${CMD}${RESET}"
+    $CMD || exit 2
+fi
 
-# Install dependencies
-sudo apt-get update
+# Ngrok
+if  $(ngrok --version > /dev/null 2>&1); then
+    printf "%s Ngrok is already installed\n" $EMOJI_NGROK
+else
+    printf "%s Installing %sNgrok%s...\n" $EMOJI_NGROK $GREEN $RESET
+    CMD='curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
+              echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list &&
+              sudo apt update && sudo apt install ngrok'
+    echo "${GREEN}${CMD}${RESET}"
+    eval $CMD || exit 2
+    echo ""
+fi
+
+# Final warnings
+printf "%s%s Some final advices: ngrok authtoken <token>...\n" $EMOJI_WARNING $YELLOW $RESET
+
+exit 0;
